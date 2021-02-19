4c7f2040fc581d48bc14eb8666d3d23d3a17620e
Zach <zshirikjian@gmail.com>
2021-02-19 09:42:23 -0500

    v1.0 Release!
    -Added in a Toggle for Muting Audio (tapping the toggle turns audio off, tapping the toggle again turns the audio on)
    -Also adjusted the position of the Snow Shape models so they don't overlap with the "Level Complete!" text 
    -Put the unused assets in the "UnusedAssets" folder (AllSnowBallsCollected SFX and the original MuteToggle script)

---
+++ Snow Shapes/Assets/UnusedAssets/MuteToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+//This script is from Info Gamer's "How to Make an Audio Toggle in Unity (Snake Cubed Winners)" tutorial on YouTube:
+//https://www.youtube.com/watch?v=FR7zDjS0mO8&list=WL&index=9
+
+[RequireComponent(typeof(Toggle))] //When we add our script to GObject, it automatically adds toggle component if it doesn't exist 
+public class MuteToggle : MonoBehaviour
+{
+    Toggle audioToggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioToggle = GetComponent<Toggle>(); 
+        if(AudioListener.volume == 0)
+        {
+            audioToggle.isOn = false;
+        }
+    }
+
+    public void ToggleAudioOnValueChange(bool audioIn)
+    {
+        if(audioIn == true)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            AudioListener.volume = 0; 
+        }
+    }
+}
